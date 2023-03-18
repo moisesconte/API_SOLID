fc6ddeec0d9db6a23e21c27cb93d63d6320585cb
Moisés Conte <mtfc.dk@hotmail.com>
2023-03-18 16:26:30 -0300

    master

---
+++ src/@types/fastify-jwt.d.ts
@@ -0,0 +1,12 @@
+import '@fastify/jwt';
+
+declare module '@fastify/jwt' {
+  export interface FastifyJWT {
+    // payload: { id: number } // payload type is used for signing and verifying
+    user: {
+      // id: number,
+      sub: string,
+      // age: number
+    } // user type is return type of `request.user` object
+  }
+}
